bdc34bd7e5bcba1fe4d26964e9b397ddd1f2f137
Zero <71897736+EliazBobadilla@users.noreply.github.com>
2021-01-21 22:02:13 -0500

    Algunas pequeñas mejoras

--- app.py
+++ app.py
@@ -1,37 +1,41 @@
-print('¿que quieres hacer?')
-print()
-print('escribe suma si quieres sumar escribe resta si quieres restar escribe multiplicar si quieres multiplicar o escribe dividir para dividir')
+# -*- coding: utf-8 -*-
+def main():
+	print('¿Que quieres hacer?\n')#El '\n' es para dejar una linea en blanco aproposito.
+	print('Escribe el nombre de la operacion que desees realizar en minusculas: ')
 
-operador = input()
-sun = 'suma'
-res = 'resta'
-multi = 'multiplicar'
-div = 'dividir'
+	operador = input()
+	sun = 'suma'
+	res = 'resta'
+	multi = 'multiplicar'
+	div = 'dividir'
 
 
-if sun == str(operador):
-	num1 = int(input('Dame el primmer numero '))
-	num2 = int(input('Dame el segundo numero '))
-	add = num1+num2
-	print(add)
+	if sun == str(operador):
+		num1 = int(input('Dame el primmer número: '))
+		num2 = int(input('Dame el segundo número: '))
+		add = num1+num2
+		print(add)
 
-elif div == str(operador):
-	d1 = int(input('Dame el primmer numero '))
-	d2 = int(input('Dame el primmer numero '))
-	dev = d1/d2
-	print(dev)
+	elif div == str(operador):
+		d1 = int(input('Dame el primmer número: '))
+		d2 = int(input('Dame el primmer número: '))
+		dev = d1/d2
+		print(dev)
 
-elif multi == str(operador):
-	n1 = int(input('Dame el primer numero '))
-	n2 = int(input('Dame el segundo numero '))
-	mul = n1*n2
-	print(mul)
+	elif multi == str(operador):
+		n1 = int(input('Dame el primer número: '))
+		n2 = int(input('Dame el segundo número: '))
+		mul = n1*n2
+		print(mul)
 
-elif res == str(operador):
-	nu1 = int(input('Dame el primer numero '))
-	nu2 = int(input('Dame el segundo numero '))
-	menos = nu1-nu2
-	print(menos)
+	elif res == str(operador):
+		nu1 = int(input('Dame el primer número: '))
+		nu2 = int(input('Dame el segundo número: '))
+		menos = nu1-nu2
+		print(menos)
 
-else:
-	print('no haz espesificado tu operacion')
+	else:
+		print('No has especificado tu operación')
+
+if __name__ == '__main__':
+	main()
